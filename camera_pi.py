4498e70aee09c0adfd0e86f94dcc18412e7d894c
Ryan Hiatt <rhiatt614@gmail.com>
2022-03-17 16:49:31 -0500

    Initial commit

--- camera_pi.py
+++ camera_pi.py
@@ -12,7 +12,7 @@ class Camera(BaseCamera):
             time.sleep(2)
 
             stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True, resize=(640, 480)):
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
