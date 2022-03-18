c53c94082f5e6ca5c11e0685d62829a936dacdc5
Ryan Hiatt <rhiatt614@gmail.com>
2022-03-18 11:39:39 -0500

    Update timelapse indexing

---
+++ testing_grounds.py
@@ -0,0 +1,6 @@
+
+image = "image00000008.jpg"
+
+file_index = int(image.split('.')[0].split('e')[1])
+
+print(file_index)
--- timelapse.py
+++ timelapse.py
@@ -1,5 +1,7 @@
 import datetime
 import time
+import glob
+import os
 import picamera
 
 from devicemanager import DeviceManager
@@ -9,10 +11,13 @@ camera = picamera.PiCamera()
 device_manager = DeviceManager()
 
 if device_manager.check_for_usb():
+    list_of_images = glob.glob(f"{device_manager.usb_path}/*.jpg")
+    latest_file = max(list_of_images, key=os.path.getctime)
+    file_index = int(latest_file.split('.')[0].split('e')[1])
 
-    i = 0
+    i = file_index + 1
     while True:
-        camera.capture(f"{device_manager.usb_path}/image{i:08d}.jpg")
+        camera.capture(f"{device_manager.usb_path}/image{i:08d}.jpg")  # image00000001.jpg
         if device_manager.check_usb_capacity() < 10:
             break
         else:
