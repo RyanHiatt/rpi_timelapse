2a7209f1b75eb42c56453161b5d6b8db51a9b629
Ryan Hiatt <rhiatt614@gmail.com>
2022-03-23 16:28:50 -0500

    Update timelapse indexing

--- testing_grounds.py
+++ testing_grounds.py
@@ -1,6 +1,9 @@
+import glob
+import os
 
-image = "image00000008.jpg"
-
-file_index = int(image.split('.')[0].split('e')[1])
-
+list_of_images = glob.glob(f"/media/ryanhiatt/D1D3-3025/*.jpg")
+latest_file = max(list_of_images, key=os.path.getctime)
+print(latest_file)
+print(latest_file.split('.')[0].split('image')[0])
+file_index = int(latest_file.split('.')[0].split('image')[1])
 print(file_index)
--- timelapse.py
+++ timelapse.py
@@ -13,7 +13,7 @@ device_manager = DeviceManager()
 if device_manager.check_for_usb():
     list_of_images = glob.glob(f"{device_manager.usb_path}/*.jpg")
     latest_file = max(list_of_images, key=os.path.getctime)
-    file_index = int(latest_file.split('.')[0].split('e')[1])
+    file_index = int(latest_file.split('.')[0].split('image')[1])
 
     i = file_index + 1
     while True:
